aedd763a1b70d7c5dbc3741d485ba339ee18b1cf
NikolayMarushkin <nikolaymarushkin94@gmail.com>
2019-02-09 12:24:11 +0300

    Update Source.cpp

--- L1miss_L2hit_to_L1hit/Source.cpp
+++ L1miss_L2hit_to_L1hit/Source.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 #include <ctime>
-#include <omp.h>
 
 using namespace std;
 
-#define SIZE_MATRIX 1000
-#define BLOCK_SIZE_MATRIX 250 // L1 miss - 250, no miss 1000
+#define SIZE_MATRIX 2000
+#define BLOCK_SIZE_MATRIX 250
 #define GRID_SIZE_MATRIX int(SIZE_MATRIX / double(BLOCK_SIZE_MATRIX))
 
-int 
-MatrixA[SIZE_MATRIX][SIZE_MATRIX] = { 0 }, 
-MatrixB[SIZE_MATRIX][SIZE_MATRIX] = { 0 }, 
-MatrixC[SIZE_MATRIX][SIZE_MATRIX] = { 0 };
+int MatrixA[SIZE_MATRIX * SIZE_MATRIX] = { 0 };
+int MatrixB[SIZE_MATRIX * SIZE_MATRIX] = { 0 };
+int MatrixC[SIZE_MATRIX * SIZE_MATRIX] = { 0 };
 
 
 int main() {
@@ -20,11 +18,11 @@ int main() {
 	{
 		for (int j = 0; j < SIZE_MATRIX; j++)
 		{
-			MatrixA[i][j] = i * SIZE_MATRIX + j + 1;
-			MatrixB[i][j] = i * SIZE_MATRIX + j + 1;
+			MatrixA[i * SIZE_MATRIX + j] = i + j + 1;
+			MatrixB[i * SIZE_MATRIX + j] = i * SIZE_MATRIX + j + 1;
 		}
 	}
-	
+
 	start_t = clock();
 	for (int n = 0; n < GRID_SIZE_MATRIX; n++)
 		for (int m = 0; m < GRID_SIZE_MATRIX; m++)
@@ -32,8 +30,11 @@ int main() {
 				for (int i = n * BLOCK_SIZE_MATRIX; i < (n + 1) * BLOCK_SIZE_MATRIX; i++)
 					for (int j = m * BLOCK_SIZE_MATRIX; j < (m + 1) * BLOCK_SIZE_MATRIX; j++)
 						for (int k = iter * BLOCK_SIZE_MATRIX; k < (iter + 1) * BLOCK_SIZE_MATRIX; k++)
-							MatrixC[i][j] += MatrixA[i][k] * MatrixB[k][j];
+							MatrixC[i * SIZE_MATRIX + j] = MatrixC[i * SIZE_MATRIX + j] + MatrixA[i * SIZE_MATRIX + k] * MatrixB[k * SIZE_MATRIX + j];
+
 	end_t = clock();
+
 	cout << (float)(end_t - start_t) / CLOCKS_PER_SEC << " sec. BlockMatrixMultiplication" << endl;
+
 	system("Pause");
 }
